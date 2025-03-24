90be1cfc77b4b60eddb44d69e03ae62f53bce7f7
unknown <moniamonika.b02@gmail.com>
2025-03-24 18:34:23 +0100

    Add filters to Cars

--- CarService/Model/Car.cs
+++ CarService/Model/Car.cs
@@ -1,12 +1,20 @@
+using Sieve.Attributes;
+
 namespace CarService.Model;
 
 public class Car
 {   
     public Guid Id { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string Brand { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string Model { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string RegistrationNumber { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string Vin { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public int Year { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public ICollection<CarCustomer> CarCustomers { get; set; } = new List<CarCustomer>();
 }
